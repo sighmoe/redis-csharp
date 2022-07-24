9d8396a8d8925b61e7dd98198863ef03ff0ca111
Kevin <sighmoe@gmail.com>
2022-07-24 15:12:58 -0700

    stage 2 - respond to ping.

--- src/Server.cs
+++ src/Server.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -7,4 +8,9 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-server.AcceptSocket(); // wait for client
+Socket client = server.AcceptSocket(); // wait for client
+
+String response = "+PONG\r\n";
+
+Byte[] responseBytes = Encoding.ASCII.GetBytes(response);
+int i = client.Send(responseBytes);
