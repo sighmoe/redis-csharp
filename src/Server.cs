7b99861eb062e0a3a499dd69ccfbd8f34a83d280
Kevin <sighmoe@gmail.com>
2022-07-24 15:16:48 -0700

    stage 3 - respond to multiple client calls.

--- src/Server.cs
+++ src/Server.cs
@@ -2,15 +2,90 @@
 using System.Net.Sockets;
 using System.Text;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
+var ParseLength = (Byte[] bytes, int start) =>
+{
+  int len = 0;
+  int i = start + 1;
+  int consumed = 1;
+
+  // 0x0D is '\r' hex value
+  while (bytes[i] != 0x0D)
+  {
+    len = (len * 10) + (bytes[i] - '0');
+    i++;
+    consumed++;
+  }
+
+  // consumed + 2 to account for ending '\r\n'
+  return (len, consumed + 2);
+};
+
+var ParseBulkStr = (Byte[] bytes, int start) =>
+{
+  var (len, consumed) = ParseLength(bytes, start);
+  Byte[] payload = new Byte[len];
+  Array.Copy(bytes, start + consumed, payload, 0, len);
+  var parsed = Encoding.ASCII.GetString(payload);
+  Console.WriteLine("ParsedBulkStr: {0}", parsed);
+  return (parsed, consumed + len);
+};
+
+var ParseClientMsg = (Byte[] msg) =>
+{
+  // Console.WriteLine("Client msg: {0}", Encoding.ASCII.GetString(msg));
+
+  var (bulkStrsToParse, cursor) = ParseLength(msg, 0);
+  List<String> bulkStrs = new List<string>();
+
+  for (int i = 0; i < bulkStrsToParse; i++)
+  {
+    var (str, consumed) = ParseBulkStr(msg, cursor);
+    bulkStrs.Add(str);
+    cursor += consumed;
+  }
+
+  return bulkStrs;
+};
+
+var ProcessPing = (String s) =>
+{
+  return s.ToLower() switch
+  {
+    "ping" => "+PONG\r\n",
+    _ => String.Format("${0}\r\n{1}\r\n", s.Length - 5, s.Substring(5)),
+  };
+};
+
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
 Socket client = server.AcceptSocket(); // wait for client
 
-String response = "+PONG\r\n";
+while (true)
+{
+  try
+  {
+    Byte[] buf = new Byte[client.SendBufferSize];
+    var bytesRead = client.Receive(buf);
+    if (bytesRead < buf.Length)
+    {
+      Array.Resize(ref buf, bytesRead);
+    }
 
-Byte[] responseBytes = Encoding.ASCII.GetBytes(response);
-int i = client.Send(responseBytes);
+    List<string> commands = ParseClientMsg(buf);
+    foreach (string command in commands)
+    {
+      Console.WriteLine("Executing command: {0}", command);
+      var response = command.ToLower() switch
+      {
+        var s when s.StartsWith("ping") => ProcessPing(s),
+        _ => throw new ArgumentException(String.Format("Received unknown redis command: {0}", command)),
+      };
+      client.Send(Encoding.ASCII.GetBytes(response));
+    }
+  }
+  catch (Exception e)
+  {
+    Console.WriteLine("Caught exception: {0}", e.ToString());
+  }
+}
